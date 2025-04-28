0592ca0d848e3591a662886ac425ad922c7cba89
DavidGMont <davidgarmo@outlook.com>
2025-04-27 23:33:51 -0500

    feat(product): Implement equals, hashCode, and toString methods in ProductEntity

--- src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/ProductEntity.java
+++ src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/ProductEntity.java
@@ -9,6 +9,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Locale;
+import java.util.Objects;
 
 @Entity
 @Table(name = "PRODUCT")
@@ -44,4 +48,36 @@ public class ProductEntity {
     @Column(columnDefinition = "VARCHAR(1000)", nullable = false)
     @NotBlank(message = "Product thumbnail cannot be null or empty.")
     private String thumbnail;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ProductEntity that = (ProductEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.ENGLISH,
+                """
+                        {
+                            id: %s,
+                            name: "%s",
+                            description: "%s",
+                            price: %.2f,
+                            available: %s,
+                            thumbnail: "%s"
+                        }
+                        """,
+                id, name, description, price, available, thumbnail);
+    }
 }
