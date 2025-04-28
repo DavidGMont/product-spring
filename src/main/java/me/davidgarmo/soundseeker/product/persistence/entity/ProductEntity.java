6dfd7c5e50743d1242b6f95b906ba4e99019eaa4
DavidGMont <davidgarmo@outlook.com>
2025-04-27 19:15:52 -0500

    feat(product): Add ProductEntity class with JPA annotations

---
+++ src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/ProductEntity.java
@@ -0,0 +1,25 @@
+package me.davidgarmo.soundseeker.product.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "PRODUCT")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String brand;
+    private Double price;
+    private Boolean available;
+    private String thumbnail;
+}
