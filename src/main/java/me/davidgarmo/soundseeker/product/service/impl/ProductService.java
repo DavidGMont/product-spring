18726e4be64b77d10d7e929e70e64e9a5ccd0c9a
DavidGMont <davidgarmo@outlook.com>
2025-04-28 18:26:08 -0500

    feat(product): Inject ProductRepository and ProductMapper into ProductService

--- src/main/java/me/davidgarmo/soundseeker/product/service/impl/ProductService.java
+++ src/main/java/me/davidgarmo/soundseeker/product/service/impl/ProductService.java
@@ -1,13 +1,26 @@
 package me.davidgarmo.soundseeker.product.service.impl;
 
+import me.davidgarmo.soundseeker.product.persistence.entity.ProductEntity;
+import me.davidgarmo.soundseeker.product.persistence.mapper.ProductMapper;
+import me.davidgarmo.soundseeker.product.persistence.repository.ProductRepository;
 import me.davidgarmo.soundseeker.product.service.ICrudService;
 import me.davidgarmo.soundseeker.product.service.dto.ProductDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductService implements ICrudService<ProductDto> {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
+    }
+
     @Override
     public ProductDto save(ProductDto productDto) {
         return null;
