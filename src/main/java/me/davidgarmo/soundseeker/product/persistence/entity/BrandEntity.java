31f568eca32d7dac70f933fc665830eee4f1d34c
DavidGMont <davidgarmo@outlook.com>
2025-04-29 02:58:05 -0500

    feat(brand): Enhance BrandEntity with additional fields and constraints

--- src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/BrandEntity.java
+++ src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/BrandEntity.java
@@ -15,9 +15,18 @@ import lombok.Setter;
 public class BrandEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
+
+    @Column(length = 60, nullable = false, unique = true)
     private String name;
+
+    @Column(length = 1000)
     private String description;
+
+    @Column(length = 1000)
     private String thumbnail;
+
+    @Column(columnDefinition = "TINYINT", nullable = false)
     private Boolean available = true;
 }
