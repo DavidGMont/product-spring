0872001094d7c54f2b7a050769b33cd5b7f6c788
DavidGMont <davidgarmo@outlook.com>
2025-04-29 02:51:48 -0500

    feat(brand): Add BrandEntity to represent brand information in the database

---
+++ src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/BrandEntity.java
@@ -0,0 +1,23 @@
+package me.davidgarmo.soundseeker.product.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "BRAND")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BrandEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String thumbnail;
+    private Boolean available = true;
+}
