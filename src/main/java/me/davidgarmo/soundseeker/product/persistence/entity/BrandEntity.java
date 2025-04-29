9d2429605d61a48ba12f055279de0b487900e6d2
DavidGMont <davidgarmo@outlook.com>
2025-04-29 03:04:41 -0500

    feat(brand): Add validation constraints to BrandEntity fields

--- src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/BrandEntity.java
+++ src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/BrandEntity.java
@@ -1,6 +1,9 @@
 package me.davidgarmo.soundseeker.product.persistence.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,14 +22,19 @@ public class BrandEntity {
     private Long id;
 
     @Column(length = 60, nullable = false, unique = true)
+    @NotBlank(message = "Brand name cannot be null or empty.")
+    @Size(max = 60, message = "Brand name cannot exceed 60 characters.")
     private String name;
 
     @Column(length = 1000)
+    @Size(max = 1000, message = "Brand description cannot exceed 1000 characters.")
     private String description;
 
     @Column(length = 1000)
+    @Size(max = 1000, message = "Brand thumbnail cannot exceed 1000 characters.")
     private String thumbnail;
 
     @Column(columnDefinition = "TINYINT", nullable = false)
+    @NotNull(message = "Brand availability cannot be null.")
     private Boolean available = true;
 }
