24adcb20a6a36a333de06e301b24a3ed7f037980
DavidGMont <davidgarmo@outlook.com>
2025-04-29 18:41:31 -0500

    feat(brand): Add IBrandService interface for BrandDto CRUD operations

---
+++ src/main/java/me/davidgarmo/soundseeker/product/service/IBrandService.java
@@ -0,0 +1,6 @@
+package me.davidgarmo.soundseeker.product.service;
+
+import me.davidgarmo.soundseeker.product.service.dto.BrandDto;
+
+public interface IBrandService extends ICrudService<BrandDto> {
+}
