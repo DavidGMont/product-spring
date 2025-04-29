8590da5413abd8f81b4b892111740be2984364b8
DavidGMont <davidgarmo@outlook.com>
2025-04-29 03:22:12 -0500

    feat(brand): Implement equals, hashCode, and toString methods in BrandEntity

--- src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/BrandEntity.java
+++ src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/BrandEntity.java
@@ -8,6 +8,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Locale;
+import java.util.Objects;
 
 @Entity
 @Table(name = "BRAND")
@@ -37,4 +41,35 @@ public class BrandEntity {
     @Column(columnDefinition = "TINYINT", nullable = false)
     @NotNull(message = "Brand availability cannot be null.")
     private Boolean available = true;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        BrandEntity that = (BrandEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.ENGLISH,
+                """
+                        {
+                            "id": %d,
+                            "name": "%s",
+                            "description": "%s",
+                            "thumbnail": "%s",
+                            "available": %b
+                        }
+                        """,
+                id, name, description, thumbnail, available);
+    }
 }
