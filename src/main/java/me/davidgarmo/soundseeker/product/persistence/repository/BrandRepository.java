6599b3cdba05fa5d24b1ac9ff6a44b0e9d78a442
DavidGMont <davidgarmo@outlook.com>
2025-04-29 19:44:05 -0500

    feat(brand): Add existsByNameIgnoreCase method to BrandRepository

--- src/main/java/me/davidgarmo/soundseeker/product/persistence/repository/BrandRepository.java
+++ src/main/java/me/davidgarmo/soundseeker/product/persistence/repository/BrandRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BrandRepository extends ListCrudRepository<BrandEntity, Long> {
+    boolean existsByNameIgnoreCase(String name);
 }
