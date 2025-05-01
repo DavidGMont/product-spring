3e95d7df223b2d2a8c6ca0b9bc328edbabcafef7
DavidGMont <davidgarmo@outlook.com>
2025-05-01 18:36:45 -0500

    feat(file): Add FileUploadConfig for multipart file upload configuration

---
+++ src/main/java/me/davidgarmo/soundseeker/product/web/config/FileUploadConfig.java
@@ -0,0 +1,25 @@
+package me.davidgarmo.soundseeker.product.web.config;
+
+import me.davidgarmo.soundseeker.product.service.IFileUploadService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class FileUploadConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Bean
+    CommandLineRunner init(IFileUploadService fileUploadService) {
+        return args -> {
+            fileUploadService.init();
+        };
+    }
+}
