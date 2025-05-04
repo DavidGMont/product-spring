4759c3d7576663a39a77ad747639c0a2e0510778
DavidGMont <davidgarmo@outlook.com>
2025-05-04 00:34:34 -0500

    feat(category): Add ICategoryService interface for CategoryDto CRUD operations

---
+++ src/main/java/me/davidgarmo/soundseeker/product/service/ICategoryService.java
@@ -0,0 +1,6 @@
+package me.davidgarmo.soundseeker.product.service;
+
+import me.davidgarmo.soundseeker.product.service.dto.CategoryDto;
+
+public interface ICategoryService extends ICrudService<CategoryDto> {
+}
