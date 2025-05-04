cac108af8651764e8b3093f69f68197a2461481f
DavidGMont <davidgarmo@outlook.com>
2025-05-04 02:18:04 -0500

    feat(category): Implement save method in CategoryService with name existence check

--- src/main/java/me/davidgarmo/soundseeker/product/service/impl/CategoryService.java
+++ src/main/java/me/davidgarmo/soundseeker/product/service/impl/CategoryService.java
@@ -1,5 +1,8 @@
 package me.davidgarmo.soundseeker.product.service.impl;
 
+import me.davidgarmo.soundseeker.product.persistence.entity.CategoryEntity;
+import me.davidgarmo.soundseeker.product.persistence.mapper.CategoryMapper;
+import me.davidgarmo.soundseeker.product.persistence.repository.CategoryRepository;
 import me.davidgarmo.soundseeker.product.service.ICategoryService;
 import me.davidgarmo.soundseeker.product.service.dto.CategoryDto;
 import org.springframework.stereotype.Service;
@@ -8,9 +11,22 @@ import java.util.List;
 
 @Service
 public class CategoryService implements ICategoryService {
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryService(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
     @Override
     public CategoryDto save(CategoryDto categoryDto) {
-        return null;
+        if (this.categoryRepository.existsByNameIgnoreCase(categoryDto.name())) {
+            throw new IllegalArgumentException("Category name already exists.");
+        }
+        CategoryEntity category = this.categoryMapper.toEntity(categoryDto);
+        CategoryEntity savedCategory = this.categoryRepository.save(category);
+        return this.categoryMapper.toDto(savedCategory);
     }
 
     @Override
