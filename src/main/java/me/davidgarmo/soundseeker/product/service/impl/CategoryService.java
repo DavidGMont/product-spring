46a7f59ae5c7d4846008cf376ef0b09cb0c12d6b
DavidGMont <davidgarmo@outlook.com>
2025-05-04 02:38:13 -0500

    feat(category): Implement update method in CategoryService with validation checks

--- src/main/java/me/davidgarmo/soundseeker/product/service/impl/CategoryService.java
+++ src/main/java/me/davidgarmo/soundseeker/product/service/impl/CategoryService.java
@@ -47,7 +47,15 @@ public class CategoryService implements ICategoryService {
 
     @Override
     public CategoryDto update(CategoryDto categoryDto) {
-        return null;
+        if (categoryDto.id() == null) {
+            throw new IllegalArgumentException("Category ID cannot be null.");
+        }
+        if (this.categoryRepository.existsByNameIgnoreCaseAndIdNot(categoryDto.name(), categoryDto.id())) {
+            throw new IllegalArgumentException("Category name already exists.");
+        }
+        CategoryEntity categoryToUpdate = this.categoryMapper.toEntity(categoryDto);
+        CategoryEntity updatedCategory = this.categoryRepository.save(categoryToUpdate);
+        return this.categoryMapper.toDto(updatedCategory);
     }
 
     @Override
