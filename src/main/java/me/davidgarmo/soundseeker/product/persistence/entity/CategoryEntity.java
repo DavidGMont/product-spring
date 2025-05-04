1fb953a7142f0057c89fcaa92b6bf103844e9188
DavidGMont <davidgarmo@outlook.com>
2025-05-03 22:35:10 -0500

    feat(category): Add CategoryEntity class for product categorization

---
+++ src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/CategoryEntity.java
@@ -0,0 +1,23 @@
+package me.davidgarmo.soundseeker.product.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "CATEGORY")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String thumbnail;
+    private Boolean available = true;
+}
