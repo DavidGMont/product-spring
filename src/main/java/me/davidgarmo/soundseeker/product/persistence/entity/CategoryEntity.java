a4d4c130e6c4d984a342d22d7fc9b568810264cb
DavidGMont <davidgarmo@outlook.com>
2025-05-03 22:39:17 -0500

    feat(category): Add validation constraints to CategoryEntity fields

--- src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/CategoryEntity.java
+++ src/main/java/me/davidgarmo/soundseeker/product/persistence/entity/CategoryEntity.java
@@ -1,6 +1,8 @@
 package me.davidgarmo.soundseeker.product.persistence.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,14 +21,19 @@ public class CategoryEntity {
     private Long id;
 
     @Column(length = 60, nullable = false, unique = true)
+    @NotBlank(message = "Category name cannot be null or empty.")
+    @Size(max = 60, message = "Category name cannot exceed 60 characters.")
     private String name;
 
     @Column(length = 1000)
+    @Size(max = 1000, message = "Category description cannot exceed 1000 characters.")
     private String description;
 
     @Column(length = 1000)
+    @Size(max = 1000, message = "Category thumbnail cannot exceed 1000 characters.")
     private String thumbnail;
 
     @Column(columnDefinition = "TINYINT", nullable = false)
+    @NotBlank(message = "Category availability cannot be null.")
     private Boolean available = true;
 }
