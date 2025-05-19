152a4bcf9c37f20a9d83bd9650f2d5c85b769b9a
DavidGMont <davidgarmo@outlook.com>
2025-05-19 01:38:14 -0500

    feat(security): Enable CORS support in security filter chain

--- src/main/java/me/davidgarmo/soundseeker/product/web/config/SecurityConfig.java
+++ src/main/java/me/davidgarmo/soundseeker/product/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package me.davidgarmo.soundseeker.product.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,6 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         matcherRegistry -> matcherRegistry
                                 .anyRequest()
