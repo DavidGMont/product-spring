2724c3fb46be2b5e59157036a96e04c38229e224
DavidGMont <davidgarmo@outlook.com>
2025-05-22 22:07:13 -0500

    test(brand): Add BrandRepositoryTest for repository layer validation

---
+++ src/test/java/me/davidgarmo/soundseeker/product/persistence/repository/BrandRepositoryTest.java
@@ -0,0 +1,38 @@
+package me.davidgarmo.soundseeker.product.persistence.repository;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class BrandRepositoryTest {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @Autowired
+    private BrandRepository brandRepository;
+
+    @DynamicPropertySource
+    static void setUpDynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> "jdbc:h2:mem:product-test");
+        registry.add("spring.datasource.username", () -> "sa");
+        registry.add("spring.datasource.password", () -> "");
+        registry.add("spring.jpa.defer-datasource-initialization", () -> false);
+        registry.add("spring.sql.init.mode", () -> "never");
+        registry.add("spring.jpa.show-sql", () -> false);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.brandRepository.deleteAll();
+        LOGGER.debug("✔ Database cleaned up.");
+    }
+}
